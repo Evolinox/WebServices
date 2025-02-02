f5534fca17553c8879827a93ed38f3fb1fd89b6a
Tim Kerl <tim.kerl.02@gmx.de>
2025-02-02 11:37:27 +0100

    Adding first Version of Product-API with PostgreSQL Db

---
+++ backend/src/main/java/nutritionAPI/ProductRepository.java
@@ -0,0 +1,18 @@
+package nutritionAPI;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<Product> {
+    public Optional<Product> findByName(String name) {
+        return find("name", name).firstResultOptional();
+    }
+
+    public List<Product> findAllProducts() {
+        return listAll();
+    }
+}
