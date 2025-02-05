1d5803a2f8f6f62dde6b0e059320f368a3fcf209
Tim Kerl <tim.kerl.02@gmx.de>
2025-02-05 12:53:02 +0100

    structured Router for tracker Api with router groups

--- backend/tracker/controller/router.go
+++ backend/tracker/controller/router.go
@@ -8,9 +8,12 @@ import (
 func RouteController(productHandler *handler.ProductHandler) {
 	router := gin.Default()
 
-	router.GET("/tracker/products", productHandler.GetProducts)
-	router.GET("/tracker/products/:id", productHandler.GetProductByID)
-	router.GET("/tracker/products/name/:name", productHandler.GetProductByName)
+	tracker := router.Group("/tracker")
+
+	productRouter := tracker.Group("/products")
+	productRouter.GET("/", productHandler.GetProducts)
+	productRouter.GET("/:id", productHandler.GetProductByID)
+	productRouter.GET("/name/:name", productHandler.GetProductByName)
 
 	router.Run("localhost:8082")
 }
