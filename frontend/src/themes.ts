4e2e6518440fa9c3ef828011304d6454313258df
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2025-02-06 20:14:17 +0100

    initial theme set to dark

--- frontend/src/themes.ts
+++ frontend/src/themes.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 
 export type ColorSchemeValue = 'light' | 'dark';
 
-export const theme = ref<ColorSchemeValue>(localStorage.getItem('theme') as ColorSchemeValue || 'light')
+export const theme = ref<ColorSchemeValue>(localStorage.getItem('theme') as ColorSchemeValue || 'dark')
 
 export function toggleTheme() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
