34279690e48c7734beaddfcec7576614222a8ffe
Tim Kerl <tim.kerl.02@gmx.de>
2025-02-11 13:16:45 +0100

    added go first parameter for swagger

--- backend/tracker/controller/router.go
+++ backend/tracker/controller/router.go
@@ -2,8 +2,23 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"tracker/controller/handler"
 )
+
+//	@title			Nutrition Tracker API
+//	@version		1.0
+//	@description	This is the Documentation of the Nutrition Tracker API, which contains 3 external APIs. The main Goal of the API is to get insights in your nutrition and helps you to organize your shopping and your meetings.
+
+//	@contact.name	Tim Kerl
+//	@contact.email	tim.kerl.23@lehre.mosbach.dhbw.de
+
+//	@host		localhost:8082
+//	@BasePath	/tracker/
+
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
 
 func RouteController(
 	productHandler *handler.ProductHandler,
@@ -34,6 +49,8 @@ func RouteController(
 	settingsRouter := tracker.Group("/settings")
 	settingsRouter.GET("/", settingsHandler.GetSettings)
 	settingsRouter.PATCH("/", settingsHandler.UpdateSettings)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	err := router.Run("localhost:8082")
 	if err != nil {
