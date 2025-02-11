5862f67f6dfa0c4680b9fd7a9dd6b31235698c58
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2025-02-11 15:46:32 +0100

    Adjust close button of the addComponent-popUp

--- frontend/src/components/AddComponent.vue
+++ frontend/src/components/AddComponent.vue
@@ -1,7 +1,10 @@
+<script lang="ts" setup>
+</script>
+
 <template>
   <div class="add-component__overlay" @click.self="$emit('close')">
     <div class="add-component__content">
-      <button @click="$emit('close')">Close</button>
+      <img class="add-component__close-button" src="../assets/plus.svg" @click="$emit('close')"/>
       <ul>
         <li>Lebensmittel</li>
         <li>Kalendereintrag</li>
@@ -10,32 +13,47 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-</script>
-
 <style scoped>
 .add-component__overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
 .add-component__content {
-    position: fixed;
-    bottom: calc(var(--nav-bar__height) + 0.5em);
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: var(--background-color--secondary);
-    padding: 1em;
-    border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: calc(var(--nav-bar__height) + 0.5em);
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: var(--background-color--secondary);
+  padding: 1em;
+  border-radius: 8px;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  padding: 0.5em;
+  cursor: pointer;
 }
 li:hover {
-    color: var(--accent-color--primary);
+  color: var(--accent-color--primary);
+}
+.add-component__close-button {
+  width: 20px;
+  height: 20px;
+  margin-left: auto;
+  margin-right: 0;
+  rotate: 45deg;
+  cursor: pointer;
 }
 </style>
