aa0f008da2734ddeb04eb79f8ee3b16e3657ef75
Tim Kerl <tim.kerl.02@gmx.de>
2025-03-02 19:00:44 +0100

    added endpoints, handler and Repository functions to consume calendarApi

--- backend/tracker/controller/repositories/calendarRepository.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-type CalendarAPIRepository struct {
-	baseURL string
-}
-
-func NewCalendarAPIRepository(baseURL string) *CalendarAPIRepository {
-	return &CalendarAPIRepository{baseURL: baseURL}
-}
