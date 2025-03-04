258403382c0daff5fd794a9e55690407179b43b9
Tim Kerl <tim.kerl.02@gmx.de>
2025-03-04 08:20:07 +0100

    refactor error handling for time format validator in handlers

--- backend/tracker/controller/helper/dateFormatValidator.go
+++ backend/tracker/controller/helper/dateFormatValidator.go
@@ -10,7 +10,7 @@ func IsValidDateFormat(date string) bool {
 
 func IsValidTimeFormat(time string) bool {
 	//check time format from 00:00 to 23:59
-	timePattern := `^(?:[01]\d|2[0-3])-[0-5]\d$`
+	timePattern := `^(?:[01]\d|2[0-3]):[0-5]\d$`
 	matched, _ := regexp.MatchString(timePattern, time)
 	return matched
 }
