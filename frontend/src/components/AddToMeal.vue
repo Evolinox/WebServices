b6327319585fa4c42dc98f067ae33e2e9fa84eb3
NicoOblaender <nicooblaender@gmail.com>
2025-03-06 16:06:09 +0100

    Rename ShowMeal to AddToMeal and style and fetch the data to DiaryView and pass to AddToMeal component

---
+++ frontend/src/components/AddToMeal.vue
@@ -0,0 +1,203 @@
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue';
+import plusSvg from '../assets/plus.svg?raw';
+import BASE_URL from '../baseUrl';
+
+interface ProductInfo {
+  ID: number;
+  Name: string;
+  Brand: string;
+  CaloriesPer100Grams: number;
+  ProteinsInGrams: number;
+  FatsInGrams: number;
+  CarbsInGrams: number;
+}
+
+const props = defineProps<{ meal: string, allProducts: ProductInfo[], day: string }>();
+const products = ref<ProductInfo[]>(props.allProducts);
+const selectedMeal = ref(props.meal);
+const emit = defineEmits(['close']);
+
+function addProduct(id: number) {
+  if (!weight.value) {
+    console.log('No weight given');
+    return;
+  }
+  console.log('Add', weight.value, 'g of product to', selectedMeal.value, 'with backenID:', id, 'to', props.day);
+  const addedProduct: ProductInfo = products.value[id - 1];
+  fetch(BASE_URL+'/consume/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Product: {
+        ID: addedProduct.ID,
+        Name: addedProduct.Name,
+        Brand: addedProduct.Brand,
+        CaloriesPer100Grams: addedProduct.CaloriesPer100Grams,
+        ProteinsInGrams: addedProduct.ProteinsInGrams,
+        FatsInGrams: addedProduct.FatsInGrams,
+        CarbsInGrams: addedProduct.CarbsInGrams,
+      },
+      meal: selectedMeal.value,
+      weight: weight.value,
+      day: props.day,
+    }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    emit('close');
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  // emit('close');
+}
+
+const weight = ref<number | null>(null);
+const handleWeightInput = (event: Event) => {
+  const input = event.target as HTMLInputElement
+  input.value = input.value.replace(/[^0-9]/g, "");
+  if (input.value.length > 5) {
+    input.value = input.value.slice(0, 5);
+  }
+  console.log('Input:', input.value);
+  weight.value = parseInt(input.value);
+  console.log('Weight:', weight.value);
+  
+}
+</script>
+
+<template>
+  <div class="add-meal__overlay" @click.self="$emit('close')" style="width: 100%;">
+    <div class="add-meal__card">
+      <div class="add-meal__card-header">
+        <h2>Füge Produkt hinzu</h2>
+        <div class="add-meal__close-button" v-html="plusSvg" @click="$emit('close')"></div>
+      </div>
+      <div class="add-meal__card-content">
+        <select name="meal-select" v-model="selectedMeal">
+          <option value="Frühstück">Frühstück</option>
+          <option value="Mittagessen">Mittagessen</option>
+          <option value="Abendessen">Abendessen</option>
+          <option value="Snack">Snack</option>
+        </select>
+        <div class="add-meal__product-list">
+          <div v-for="product in products" class="add-meal__product">
+            <div class="product__description">
+              <p>{{ product.Name }} - {{ product.Brand }}</p>
+              <p style="color: var(--text-color--secondary);">{{ product.CaloriesPer100Grams }} kcal / 100g</p>
+            </div>
+            <div class="product-list__icon--add" v-html="plusSvg" @click="addProduct(product.ID)"></div>
+          </div>
+        </div>
+        <input type="text" placeholder="Menge in Gramm" inputmode="numeric" pattern="[0-9]*" @input="handleWeightInput"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.add-meal__overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+}
+
+.add-meal__card {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  min-width: 370px;
+  background-color: var(--background-color--secondary);
+  padding: 1em;
+  border-radius: 8px;
+}
+
+.add-meal__card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.add-meal__card-header h2 {
+  margin-left: 5px;
+}
+
+.add-meal__close-button {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  margin-left: 30px;
+  margin-top: 0px;
+  scale: 1.2;
+  fill: var(--icon-color);
+}
+
+.add-meal__card-content {
+  padding: 0px 35px;
+  text-align: center;
+}
+
+.add-meal__product {
+  display: flex;
+  margin: 10px 0;
+}
+.add-meal__product p {
+  margin: 3px 0px;
+}
+.product__description {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  text-align: start;
+}
+.product-list__icon--add {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--button__background-color);
+  height: 35px;
+  width: 35px;
+  scale: 0.9;
+  border-radius: 90px;
+  margin-left: auto;
+  cursor: pointer;
+}
+.product-list__icon--add > svg {
+  height: 50px;
+  width: 50px;
+  scale: 0.75;
+  fill: var(--button__text-color);
+  margin: 0;
+}
+
+
+
+
+.add-meal__card-footer {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.add-meal__add-article--button {
+  height: 35px;
+  width: 35px;
+  border-radius: 90px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--button__background-color);
+  scale: 0.9;
+  fill: var(--button__text-color);
+}
+</style>
--- frontend/src/views/DiaryView.vue
+++ frontend/src/views/DiaryView.vue
@@ -1,67 +1,145 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import DayCalender from '../components/DayCalender.vue';
+import { onMounted, ref, watch, computed } from 'vue';
+import { format } from "date-fns";
 import croissantSvg from '../assets/croissant.svg?raw';
 import pizzaSvg from '../assets/pizza.svg?raw';
 import saladSvg from '../assets/salad.svg?raw';
 import appleSvg from '../assets/apple.svg?raw';
 import plusSvg from '../assets/plus.svg?raw';
-import ShowMeal from '../components/ShowMeal.vue';
+import trashSvg from '../assets/trash.svg?raw';
+import currentDay from '../day';
+import baseUrl from '../baseUrl';
+import AddMeal from '../components/AddToMeal.vue';
 
-const openMeal = ref(false)
-const currentMeal = ref('')
-const addArticle = ref(false)
-
-function showMeal(meal: string) {
-  console.log('Show meal:', meal)
-  openMeal.value = true
-  currentMeal.value = meal
+interface Product {
+  ID: number;
+  DailyProductsConsumedID: number;
+  ProductID: number;
+  ProductName: string;
+  Category: string;
+  WeightInGrams: number;
+  Calories: number;
+  ProteinsInGrams: number;
+  FatsInGrams: number;
+  CarbsInGrams: number;
 }
-function closeMeal() {
-  openMeal.value = false
-  currentMeal.value = ''
-  addArticle.value = false
+
+interface ProductInfo {
+  ID: number;
+  Name: string;
+  Brand: string;
+  CaloriesPer100Grams: number;
+  ProteinsInGrams: number;
+  FatsInGrams: number;
+  CarbsInGrams: number;
 }
 
-function add(meal: string) {
-  addArticle.value = true
-  showMeal(meal)
-  console.log('Add food to', meal)
+const openAddToMealBoolean = ref(false);
+const meal = ref('');
+const breakfastProducts = ref<Product[]>([]);
+const lunchProducts = ref<Product[]>([]);
+const dinnerProducts = ref<Product[]>([]);
+const snackProducts = ref<Product[]>([]);
+const allProducts = ref<ProductInfo[]>([]);
+
+const currentDayBackend = computed(() => {
+  return format(currentDay.value, "yyyy-MM-dd");
+});
+
+onMounted(() => {
+  loadDay();
+  loadProducts();
+})
+
+watch(currentDay, () => {
+  console.log('Diary: Day changed');
+  loadDay()
+})
+
+function openAddToMeal(newMeal: string) {
+  meal.value = newMeal;
+  console.log('Diary: Meal: ', meal.value);
+  openAddToMealBoolean.value = true;
+}
+function closeAddToMeal() {
+  openAddToMealBoolean.value = false;
+}
+
+function loadDay() {
+  console.log('Load day')
+  fetch(baseUrl + '/diary/date/' + currentDayBackend.value)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      breakfastProducts.value = data.products.filter((product: Product) => product.Category === 'Frühstück');
+      lunchProducts.value = data.products.filter((product: Product) => product.Category === 'Mittagessen');
+      dinnerProducts.value = data.products.filter((product: Product) => product.Category === 'Abendessen');
+      snackProducts.value = data.products.filter((product: Product) => product.Category === 'Snack');
+    }) 
+}
+
+function loadProducts() {
+  const url = baseUrl + '/products/';
+  console.log('Load products from ', url)
+  fetch(baseUrl + '/products/')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      allProducts.value = data;
+    })
 }
 </script>
 
 <template>
-  <div class="diary-view" style="width: calc(100% - 40px)">
+  <DayCalender class="week-kalender" />
+  <div class="diary-view" style="width: calc(100% - 40px); height: calc(100vh - var(--nav-bar__height) - var(--week-kalender__height));">
     <div class="diary__container">
       <div class="diary-header">
         <h1>Tagebuch</h1>
       </div>
       <div class="diary__content">
         <div class="diary-content__item diary-content__item--breakfast">
-          <div class="diary-content__icon diary-content__icon--smaller" v-html="croissantSvg" @click="showMeal('breakfast')"></div>
-          <h2 @click="showMeal('breakfast')">Frühstück</h2>
-          <div class="diary-content__icon--add" v-html="plusSvg" @click="add('breakfast')"></div>
+          <div class="diary-content__icon diary-content__icon--smaller" v-html="croissantSvg"></div>
+          <h2>Frühstück</h2>
+          <div class="diary-content__icon--add" v-html="plusSvg" @click="openAddToMeal('Frühstück')"></div>
         </div>
+        <ul v-if="breakfastProducts.length > 0">
+          <li v-for="product in breakfastProducts" :key="product.ID">{{ product.ProductName }}</li>
+        </ul>
         <div class="diary-content__line"></div>
         <div class="diary-content__item diary-content__item--lunch">
-          <div class="diary-content__icon diary-content__icon--bigger" v-html="pizzaSvg" @click="showMeal('lunch')"></div>
-          <h2 @click="showMeal('lunch')">Mittagessen</h2>
-          <div class="diary-content__icon--add" v-html="plusSvg" @click="add('lunch')"></div>
+          <div class="diary-content__icon diary-content__icon--bigger" v-html="pizzaSvg"></div>
+          <h2>Mittagessen</h2>
+          <div class="diary-content__icon--add" v-html="plusSvg" @click="openAddToMeal('Mittagessen')"></div>
+          <ul v-if="lunchProducts.length > 0">
+            <li v-for="product in lunchProducts" :key="product.ID">{{ product.ProductName }}</li>
+          </ul>
         </div>
         <div class="diary-content__line"></div>
         <div class="diary-content__item diary-content__item--dinner">
-          <div class="diary-content__icon diary-content__icon--bigger" v-html="saladSvg" @click="showMeal('dinner')"></div>
-          <h2 @click="showMeal('dinner')">Abendessen</h2>
-          <div class="diary-content__icon--add" v-html="plusSvg" @click="add('dinner')"></div>
+          <div class="diary-content__icon diary-content__icon--bigger" v-html="saladSvg"></div>
+          <h2>Abendessen</h2>
+          <div class="diary-content__icon--add" v-html="plusSvg" @click="openAddToMeal('Abendessen')"></div>
+          <ul v-if="dinnerProducts.length > 0">
+            <li v-for="product in dinnerProducts" :key="product.ID">
+              {{ product.ProductName }}
+              <div v-html="trashSvg"></div>
+            </li>
+          </ul>
         </div>
         <div class="diary-content__line"></div>
         <div class="diary-content__item diary-content__item--snacks">
-          <div class="diary-content__icon diary-content__icon--smaller" v-html="appleSvg" @click="showMeal('snacks')"></div>
-          <h2 @click="showMeal('snacks')">Snacks</h2>
-          <div class="diary-content__icon--add" v-html="plusSvg" @click="add('snacks')"></div>
+          <div class="diary-content__icon diary-content__icon--smaller" v-html="appleSvg"></div>
+          <h2>Snack</h2>
+          <div class="diary-content__icon--add" v-html="plusSvg" @click="openAddToMeal('Snack')"></div>
+          <ul v-if="snackProducts.length > 0">
+            <li v-for="product in snackProducts" :key="product.ID">{{ product.ProductName }}</li>
+          </ul>
         </div>
       </div>
     </div>
-    <ShowMeal v-if="openMeal" :meal="currentMeal" :add-article="addArticle" @close="closeMeal" />
+    <AddMeal v-if="openAddToMealBoolean" :meal="meal" :all-products="allProducts" :day="currentDayBackend" @close="closeAddToMeal"/>
   </div>
 </template>
 
@@ -107,6 +185,7 @@ function add(meal: string) {
   scale: 0.9;
   border-radius: 90px;
   margin-left: auto;
+  cursor: pointer;
 }
 .diary-content__icon--add > svg {
   height: 50px;
