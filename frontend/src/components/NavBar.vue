b0a0769471deee61db07a2e653ad3c948569a12e
NicoOblaender <nicooblaender@gmail.com>
2025-03-12 11:35:21 +0100

    Delete NavBar__plus

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -4,7 +4,6 @@ import { useRoute, useRouter } from 'vue-router'
 
 import dashboardSvg from '../assets/dashboard.svg?raw'
 import diarySvg from '../assets/diary.svg?raw'
-import plusSvg from '../assets/plus.svg?raw'
 import shoppingListSvg from '../assets/shoppingList.svg?raw'
 import settingsSvg from '../assets/settings.svg?raw'
 
@@ -18,7 +17,6 @@ const isActive = (path: string) => computed(() => route.name?.toString().include
   <div class="nav-bar">
     <button :class="['dashboard', 'nav-bar__item', { 'nav-bar__item--active': isActive('dashboard').value }]" v-html="dashboardSvg" @click="router.push({ name: 'dashboard' })"></button>
     <button :class="['nav-bar__item', { 'nav-bar__item--active': isActive('diary').value }, 'rightShift']" v-html="diarySvg" @click="router.push({ name: 'diary' })"></button>
-    <button :class="['plus', 'nav-bar__item']" v-html="plusSvg" @click="$emit('toggle-add-component')"></button>
     <button :class="['nav-bar__item', { 'nav-bar__item--active': isActive('shoppingList').value }]" v-html="shoppingListSvg" @click="router.push({ name: 'shoppingList' })"></button>
     <button :class="['nav-bar__item', { 'nav-bar__item--active': isActive('settings').value }]" v-html="settingsSvg" @click="router.push({ name: 'settings' })"></button>
   </div>
@@ -30,6 +28,8 @@ const isActive = (path: string) => computed(() => route.name?.toString().include
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  width: 90%;
+  max-width: 600px;
 }
 .nav-bar__item {
   height: 45px;
@@ -53,14 +53,7 @@ const isActive = (path: string) => computed(() => route.name?.toString().include
   position: relative;
   left: 2px;
 }
-.plus {
-  background-color: var(--button__background-color);
-  border-radius: 90px;
-}
 .dashboard > svg {
   scale: 0.9;
-}
-.plus > svg {
-  fill: var(--button__text-color);
 }
 </style>
